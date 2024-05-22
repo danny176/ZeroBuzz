c901ba4ce6c6e7f5652a0601c8ccc2bbde3b5dd2
danny176 <160113158+danny176@users.noreply.github.com>
2024-05-22 20:36:39 +0200

    Forside færdig, RWD complete mobile, tablet og desktop

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+//Mobile navigation//
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,3 +7,13 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+  //Mikrobryggeri teaser in viewport fadein//
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const teaser = document.querySelector('.mikrobryggeri-teaser');
+    window.addEventListener('scroll', () => {
+        const rect = teaser.getBoundingClientRect();
+        if (rect.top < window.innerHeight) {
+            teaser.classList.add('visible');
+        }
+    });
+});
