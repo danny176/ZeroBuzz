21b37090430433ed3368dda58408faf7f6dda35b
danny176 <160113158+danny176@users.noreply.github.com>
2024-05-29 22:34:56 +0200

    ZeroBuzz 1.0 , optimized SEO, page load time

--- js/js.js
+++ js/js.js
@@ -1,42 +1,66 @@
-//Mobile navigation//
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  var topnav = document.querySelector('.hamburger');
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    topnav.classList.remove('open');
-  } else {
-    x.style.display = "block";
-    topnav.classList.add('open');
+  // Mobile navigation
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    var topnav = document.querySelector('.hamburger');
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      topnav.classList.remove('open');
+    } else {
+      x.style.display = "block";
+      topnav.classList.add('open');
+    }
   }
-}
 
+  // Mikrobryggeri teaser in viewport fadein
+  document.addEventListener('DOMContentLoaded', () => {
+  const teaser = document.querySelector('.mikrobryggeri-teaser');
+  window.addEventListener('scroll', () => {
+    const rect = teaser.getBoundingClientRect();
+    if (rect.top < window.innerHeight) {
+      teaser.classList.add('visible');
+    }
+  });
 
-
-  //Mikrobryggeri teaser in viewport fadein//
-  document.addEventListener('DOMContentLoaded', (event) => {
-    const teaser = document.querySelector('.mikrobryggeri-teaser');
-    window.addEventListener('scroll', () => {
-        const rect = teaser.getBoundingClientRect();
-        if (rect.top < window.innerHeight) {
-            teaser.classList.add('visible');
-        }
+  // Vision fade in
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
     });
-});
+  }, {
+    threshold: 0.1
+  });
 
-  //Vision fade in//
-  document.addEventListener('DOMContentLoaded', function() {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, {
-        threshold: 0.1 // Adjust this value as needed
-    });
-
-    const section = document.querySelector('.image-text-container-vision');
+  const section = document.querySelector('.image-text-container-vision');
+  if (section) {
     observer.observe(section);
+  }
+
+  // Video spiller kun i viewport
+  document.querySelectorAll('.parallax-video').forEach(function(element) {
+    element.loop = true;
+  });
+  $(".parallax-video").each(function() {
+    $(this).prop({
+      controls: false,
+      controlslist: "nodownload"
+    });
+    const observer = new window.IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          if (this.paused) {
+            $(this).prop("muted", true);
+            this.play();
+          }
+        } else {
+          this.pause();
+        }
+      }, {
+        threshold: 0.7,
+      }
+    );
+    observer.observe(this);
+  });
 });
